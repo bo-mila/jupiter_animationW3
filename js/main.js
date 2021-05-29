3e933d01dc6ed459f9b7c63fceb1435346decbb7
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-05-29 15:53:16 +0300

    add glassmorfizm on first-slider, add animation keyframes of bounce on first page

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// const bounce = document.querySelector(".bounce-img-3");
+// // const bounces = document.querySelector(".bounces");
+// bounce.addEventListener("mousemove", (e) => {
+//   bounce.left = "50px";
+//   // bounce.animation = "1s bounceBlur1 100s";
+// });
