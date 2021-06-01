8f6c6e9e37efa393aea35bd4a07eb8b9105546a1
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-06-01 11:30:56 +0300

    бегущие строки при ховере осанавливаются, вторя строка сдвинута

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// const bounce = document.querySelector(".bounce-img-3");
-// // const bounces = document.querySelector(".bounces");
-// bounce.addEventListener("mousemove", (e) => {
-//   bounce.left = "50px";
-//   // bounce.animation = "1s bounceBlur1 100s";
-// });
-
 
 const firstSliderButPrev = document.querySelector(".slider-first__button--prev");
 const firstSliderButNext = document.querySelector(".slider-first__button--next");
@@ -231,7 +224,42 @@ var swiperWhy = new Swiper(".mySwiperWhy", {
 
 
 
-const reviews = document.querySelector(".reviews");
-reviews.addEventListener("mousemove", function (e) {
-  reviews.classList.add("reviews--active");
-})
+const animItems = document.querySelectorAll("._anim-items");
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add("_active");
+      } else {
+        if (!animItem.classList.contains("_anim-no-repeat")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+
+}
+
