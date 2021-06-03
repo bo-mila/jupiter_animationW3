83d39f7899150484fbdaf37cf295c1a803aa97df
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-06-03 18:36:26 +0300

    parallax of first page

--- js/main.js
+++ js/main.js
@@ -262,3 +262,111 @@ if (animItems.length > 0) {
   }, 300);
 
 }
+
+
+
+window.onload = function () {
+  const first = document.querySelector(".first");
+  // if (first) {
+  //   const bounceBlur1 = document.querySelector(".bounce-blur-1");
+  //   const bounceBlur2 = document.querySelector(".bounce-blur-2");
+  //   const bounceBlur3 = document.querySelector(".bounce-blur-3");
+  //   const bounceImg1 = document.querySelector(".bounce-img-1");
+  //   const bounceImg2 = document.querySelector(".bounce-img-2");
+  //   const bounceImg3 = document.querySelector(".bounce-img-3");
+  //   const bounceImg4 = document.querySelector(".bounce-img-4");
+  //   const bounceImg5 = document.querySelector(".bounce-img-5");
+  //   const parallax = document.querySelector(".parallax");
+
+  //   const forBounceBlur1 = 2;
+  //   const forBounceBlur2 = 3;
+  //   const forBounceBlur3 = 6;
+  //   const forBounceImg1 = 3;
+  //   const forBounceImg2 = 4;
+  //   const forBounceImg3 = 5;
+  //   const forBounceImg4 = 6;
+  //   const forBounceImg5 = 5;
+  //   const forParallax = 15;
+
+  //   const speed = 0.05;
+    
+  //   let positionX = 0, positionY = 0;
+  //   let coordXprocent = 0, coordYprocent = 0;
+
+  //   function setMouseParallaxStyle() {
+  //     const distX = coordXprocent - positionX;
+  //     const distY = coordYprocent - positionY;
+  //     positionX = positionX + (distX * speed);
+  //     positionY = positionY + (distY * speed);
+
+  //     bounceBlur1.style.cssText = `transform: translate(${-positionX / forBounceBlur1}%, ${-positionY / forBounceBlur1}%);`;
+  //     bounceBlur2.style.cssText = `transform: translate(${-positionX / forBounceBlur2}%, ${-positionY / forBounceBlur2}%);`;
+  //     bounceBlur3.style.cssText = `transform: translate(${-positionX / forBounceBlur3}%, ${-positionY / forBounceBlur3}%);`;
+  //     bounceImg1.style.cssText = `transform: translate(${-positionX / forBounceImg1}%, ${-positionY / forBounceImg1}%);`;
+  //     bounceImg2.style.cssText = `transform: translate(${-positionX / forBounceImg2}%, ${-positionY / forBounceImg2}%);`;
+  //     bounceImg3.style.cssText = `transform: translate(${-positionX / forBounceImg3}%, ${-positionY / forBounceImg3}%);`;
+  //     bounceImg4.style.cssText = `transform: translate(${-positionX / forBounceImg4}%, ${-positionY / forBounceImg4}%);`;
+  //     bounceImg5.style.cssText = `transform: translate(${positionX / forBounceImg5}%, ${-positionY / forBounceImg5}%);`;
+  //     parallax.style.cssText = `transform: translate(${-positionX / forParallax}%, ${-positionY / forParallax}%);`;
+
+  //     requestAnimationFrame(setMouseParallaxStyle);
+
+  //   }
+  //   setMouseParallaxStyle();
+
+  //   parallax.addEventListener("mousemove", function(e) {
+  //     const parallaxWidth = parallax.offsetWidth;
+  //     const parallaxHeight = parallax.offsetHeight;
+
+  //     const coordX = e.pageX - parallaxWidth / 2;
+  //     const coordY = e.pageY - parallaxHeight / 2;
+
+  //     coordXprocent = coordX / parallaxWidth * 100;
+  //     coordYprocent = coordY / parallaxHeight * 100;
+
+  //   });
+
+  // }
+  const parallax = document.querySelector(".parallax");
+  const bounceBlur1 = document.querySelector(".bounce-blur-1");
+    const bounceBlur2 = document.querySelector(".bounce-blur-2");
+    const bounceBlur3 = document.querySelector(".bounce-blur-3");
+    const bounceImg1 = document.querySelector(".bounce-img-1");
+    const bounceImg2 = document.querySelector(".bounce-img-2");
+    const bounceImg3 = document.querySelector(".bounce-img-3");
+    const bounceImg4 = document.querySelector(".bounce-img-4");
+    const bounceImg5 = document.querySelector(".bounce-img-5");
+
+  first.addEventListener("mousemove", parallaxGo);
+  function parallaxGo(e) {
+    this.querySelectorAll(".bounce").forEach(bounce => {
+      const speed = bounce.getAttribute('data-speed');
+      // const dataX = bounce.getAttribute('data-x');
+      const x = (window.innerWidth - e.pageX*speed)/100;
+      const y = (window.innerHeight - e.pageY*speed)/100;
+      bounce.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      bounce.style.transition = ".4s";
+      
+    });
+    const speed = parallax.getAttribute('data-speed');
+    const x = (window.innerWidth - e.pageX*speed)/100;
+    const y = (window.innerHeight - e.pageY*speed)/100;
+    parallax.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  }
+  first.addEventListener("mousestop", parallaxStop);
+  function parallaxStop(e) {
+    this.querySelectorAll(".bounce").forEach(bounce => {
+      // const speed = bounce.getAttribute('data-speed');
+      // // const dataX = bounce.getAttribute('data-x');
+      // const x = (window.innerWidth - e.pageX*speed)/100;
+      // const y = (window.innerHeight - e.pageY*speed)/100;
+      // bounce.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      bounce.style.transition = ".4s";
+      
+    });
+    // const speed = parallax.getAttribute('data-speed');
+    // const x = (window.innerWidth - e.pageX*speed)/100;
+    // const y = (window.innerHeight - e.pageY*speed)/100;
+    // parallax.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  }
+}
