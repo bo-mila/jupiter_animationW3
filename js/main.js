ee7a1ae0a9bd78095969d43293858610193f786e
Mila Bozhevskaya <mi.bozhev@gmail.com>
2021-06-04 13:22:12 +0300

    adaptive for full, desctop, note, tablets

--- js/main.js
+++ js/main.js
@@ -77,11 +77,16 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     fDisp[i].classList.remove("fdisplay__item--active");
     x[i].style.display = "none";
-    
+    // x[slideIndex-1].style.opacity = "0";
+    // x[i].classList.remove("slider-first__item--active");
     
   }
  fDisp[n-1].classList.add("fdisplay__item--active");
   x[slideIndex-1].style.display = "flex";
+  // x[slideIndex-1].style.transition = "opacity .4s";
+  // x[slideIndex-1].style.opacity = "1";
+  // x[slideIndex-1].classList.add("slider-first__item--active");
+
 }
 
 
